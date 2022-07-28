4d9e07ddb5bb76c551f46a53323be09e50caabb3
yonat <yonatan.semidubersky@live.biu.ac.il>
2022-07-28 22:59:26 +0300

    Implemented quick-select algorithm in "IrisAndDistance" instead of sort. Also, added "readme" file.

--- IrisAndDistance.cpp
+++ IrisAndDistance.cpp
@@ -23,26 +23,52 @@ double IrisAndDistance::getDistance() const {
     return this->distance;
 }
 
-struct comparator {
-    inline bool operator()(const IrisAndDistance &flower1, IrisAndDistance &flower2) {
-        return (flower1.getDistance() < flower2.getDistance());
-    }
-};
-
-
-
 IrisAndDistance::IrisAndDistance(IrisAndDistance *p) {
     this->flower = p->getFlower();
     this->distance = p->getDistance();
 }
-
+// help method: sorting the vector by r index -
+// greater will be in the right and smaller in the left.
+int arrange(vector<IrisAndDistance> v, int l, int r)
+{
+    int x = arr[r];
+    for (int j = l; j <= r - 1; j++) {
+        if (arr[j] <= x) {
+            swap(arr[l], arr[j]);
+            l++;
+        }
+    }
+    swap(arr[l], arr[r]);
+    return l;
+}
+// quick select algorithm - finding the k smallest element.
+int kElement(vector<IrisAndDistance> v, int left, int right, int k)
+{
+    if (k > 0 && k <= r - l + 1) {
+        // arrange around l.
+        int index = arrange(arr, l, r);
+        if (index - l == k - 1)
+            return arr[index];
+        // binary search of k:
+        if (index - l > k - 1)
+            return kElement(arr, l, index - 1, k);
+        return kElement(arr, index + 1, r,
+                           k - index + l - 1);
+    } else{
+        return -1; // wrong value for k.
+    }
+}
 
 string IrisAndDistance::kNearest(vector<IrisAndDistance> v, int k) {
-    std::sort(v.begin(), v.end(), comparator());
+    int index = kthSmallest(v,0,v.size(),k);
     int versicolor = 0;
     int virginica = 0;
     int setosa = 0;
-    for (int i = 1; i <= k; i++) {
+    for (int i = 0; i <= v.size(); i++) {
+        // if the element isn't in the k nearest neighbors.
+        if (v[i] > v[index]){
+            continue;
+        }
         if (v[i].getFlower().getType() == "Iris-versicolor") {
             versicolor++;
         }
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Exercise01
+# Ariel Houri & Yonatan Semidubersky
+
+the classification k-nearest neighbors algorithm (k-NN),
+return a class membership.
+An object is classified by a plurality vote of its neighbors,
+with the object being assigned to the class most common among its k nearest neighbors. (wikipedia)
+
+In this assignment, we implemented Classifier KNN for Iris,
+given unclassified iris and k (number),
+we evaluate his type.
+
+# Instruction:
+The data must be arranged in this formation in the project's directory:
+- classified.csv will be the data of irises
+- Unclassified.csv will be the irises to classify.
+- The output will be in: euclidean_output.csv, manhattan_output.csv, chebyshev_output.csv 
+(according to the metric).
+- The program must ber executed from Main.cpp
+# The Algorithm:
+- The main class is in charge of setting the data, writing and reading from files.
+- The flower class store data on specific flower.
+- The ClassifierKNN is in charge of arranging the data,
+and process input for finding the algorithm.
+- IrisAndDistance is an object to store iris and his distance from the unclassified iris.
+Also, it has a function that implement "quick-select" for the KNN.
