95e13b36011217629f3ae2366bce7c85f194a4e9
ariel <ArielH1300@gmail.com>
2022-07-29 13:25:24 +0300

    IrisAndDistance updated version

--- IrisAndDistance.cpp
+++ IrisAndDistance.cpp
@@ -27,32 +27,34 @@ IrisAndDistance::IrisAndDistance(IrisAndDistance *p) {
     this->flower = p->getFlower();
     this->distance = p->getDistance();
 }
+
 // help method: sorting the vector by r index -
 // greater will be in the right and smaller in the left.
 int arrange(vector<IrisAndDistance> v, int l, int r)
 {
-    int x = arr[r];
-    for (int j = l; j <= r - 1; j++) {
-        if (arr[j] <= x) {
-            swap(arr[l], arr[j]);
+    int x = v[r-1].getDistance();
+    for (int j = l; j < r - 1; j++) {
+        if (v[j].getDistance() <= x) {
+            swap(v[l], v[j]);
             l++;
         }
     }
-    swap(arr[l], arr[r]);
+    swap(v[l], v[r-1]);
     return l;
 }
+
 // quick select algorithm - finding the k smallest element.
-int kElement(vector<IrisAndDistance> v, int left, int right, int k)
+int kElement(vector<IrisAndDistance> v, int l, int r, int k)
 {
     if (k > 0 && k <= r - l + 1) {
         // arrange around l.
-        int index = arrange(arr, l, r);
+        int index = arrange(v, l, r);
         if (index - l == k - 1)
-            return arr[index];
+            return v[index].getDistance(); //not sure about the .getDistance
         // binary search of k:
         if (index - l > k - 1)
-            return kElement(arr, l, index - 1, k);
-        return kElement(arr, index + 1, r,
+            return kElement(v, l, index - 1, k);
+        return kElement(v, index + 1, r,
                            k - index + l - 1);
     } else{
         return -1; // wrong value for k.
@@ -60,13 +62,13 @@ int kElement(vector<IrisAndDistance> v, int left, int right, int k)
 }
 
 string IrisAndDistance::kNearest(vector<IrisAndDistance> v, int k) {
-    int index = kthSmallest(v,0,v.size(),k);
+    int index = kElement(v,0,v.size(),k);
     int versicolor = 0;
     int virginica = 0;
     int setosa = 0;
     for (int i = 0; i <= v.size(); i++) {
         // if the element isn't in the k nearest neighbors.
-        if (v[i] > v[index]){
+        if (v[i].getDistance() > v[index].getDistance()){
             continue;
         }
         if (v[i].getFlower().getType() == "Iris-versicolor") {
