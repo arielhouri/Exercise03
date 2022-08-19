2e282962c94ab0e7b0bd0642b94325c2d18a41f6
yonat <yonatan.semidubersky@live.biu.ac.il>
2022-08-19 23:30:28 +0300

    implemented CLI (not all because there are few commands missing).

--- CLI.hpp
+++ CLI.hpp
@@ -1,16 +1,21 @@
 //
-// Created by ariel on 8/16/2022.
+// Created by Yonatan Semidubersky on 8/16/2022.
 //
 
 #ifndef IRIS_CPP_CLI_HPP
 #define IRIS_CPP_CLI_HPP
 
 #include "Commands/Command.hpp"
-
+#include "ClassifierParameters.hpp"
 class CLI {
     Command** commands;
-
+    DefaultIO* io;
+    std::string pathFile;
+    ClassifierParameters parameters;
+    std::string results;
+    bool shouldStop;
 public:
+    CLI(DefaultIO io);
     void start();
 };
 
