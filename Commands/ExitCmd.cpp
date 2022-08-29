4d58f373fc0082ad3660e355e22fc07d0e10184b
ariel <ArielH1300@gmail.com>
2022-08-29 16:00:09 +0300

    Working version with a memory leak (see ExitCmd.cpp file)

--- Commands/ExitCmd.cpp
+++ Commands/ExitCmd.cpp
@@ -4,24 +4,22 @@
 
 #include "ExitCmd.hpp"
 
-ExitCmd::ExitCmd(DefaultIO* dio, ClassifierParameters* cp, ClassificationFiles* cf, Command** cmds) {
+ExitCmd::ExitCmd(DefaultIO* dio, ClassifierParameters* cp, ClassificationFiles* cf, Command** cmds, bool* shouldStop) {
     this->cp = cp;
     this->cf = cf;
     this->cmds = cmds;
     this->dio = dio;
+    this->shouldStop = shouldStop;
     this->description = "exit";
 }
 
 void ExitCmd::execute() {
     delete this->cp; // Frees all the memory.
     delete this->cf;
-    delete this->cmds[0];
-    delete this->cmds[1];
-    delete this->cmds[2];
-    delete this->cmds[3];
-    delete this->cmds[4];
-    delete this->cmds[5];
-    delete this->cmds[6];
-    delete this->cmds;
+    for (int i = 0; i < 7; i++) { // Frees the actual commands.
+        delete this->cmds[i];
+    }
+//    delete[] this->cmds; // Frees the commands-arr
     dio->write("$exit$");
+    *(this->shouldStop) = true;
 }
