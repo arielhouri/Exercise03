fdc38f694e459c055649b878193022a454c9e4dc
yonat <yonatan.semidubersky@live.biu.ac.il>
2022-08-31 19:25:58 +0300

    Fixed the classification. Implemented confusion matrix (also supports windows' files).

--- Commands/ConfusionMatrixCmd.cpp
+++ Commands/ConfusionMatrixCmd.cpp
@@ -1,5 +1,5 @@
 //
-// Created by Yonatan Semidubersky on 8/31/2022.
+// Created by ariel on 8/19/2022.
 //
 
 #include <fstream>
@@ -87,72 +87,20 @@ int ConfusionMatrixCmd::get(const string& key, map<string, int> types){
 }
 
 void ConfusionMatrixCmd::execute() {
-    string classified = this->files->getClassified(); // copy the classified
-    if (classified.empty()) {
-        this->dio->write("$print$Error!");
-        this->dio->read();
-        return;
-    }
-    map<string, int> realTypes = mapTypes(classified);
-    // setting the matrix:
-    std::vector<std::vector<double> > mat(realTypes.size()); // matrix builder
-    std::string delimiter = "\n";
-    size_t pos = 0;
-    std::string line;
-    std::string token = classified.substr(pos, classified.find(delimiter));
-    string type;
-    map<string, int>::iterator itr;
-    for (int i = 0; i < realTypes.size(); i++) {
-        mat[i].resize(realTypes.size());
-    }
-    for (int i = 0; i < realTypes.size(); i++) {
-        for (int j = 0; j < realTypes.size(); j++){
-            mat[i][j] = 0;
-        }
-    }
-    classified = this->files->getClassified();
-    vector<Classifiable> database = ClassifierKnn::setupDatabase(classified); // Creates the database.
-    while ((pos = classified.find(delimiter)) != std::string::npos) {
-        token = classified.substr(0, classified.find(delimiter));
-        classified.erase(0, pos + delimiter.length());
-        // windows' fixing ('\r'):
-        if (token.find("\r") != std::string::npos){
-            line = token.substr(0, token.find("\r"));
-            line += "\n";
-        }
-        else{
-            line = token;
-        }
-        type = getType(line);
-        int row = get(type,realTypes);
-        Classifiable toClass = ClassifierKnn::stringToClassifiable(&line, true);
-        ClassifierKnn classifier(database, toClass, this->classParams->getK()); // Creates its classifier.
-        string classType = classifier.classify(*(this->classParams));
-        // windows' fixing ('\r'):
-        if (classType.find("\r") != std::string::npos){
-            classType = classType.substr(0, classType.find("\r"));
-        }
-        int col = get(classType, realTypes);
-        mat[row][col] += 1;
-    }
-    string matrixPrint = "\t";
-    for (itr = realTypes.begin(); itr != realTypes.end(); itr++) {
-        matrixPrint += itr->first;
-        matrixPrint += "\t";
-    }
-    matrixPrint += "\n";
-    itr = realTypes.begin();
-    for (int i = 0; i < realTypes.size(); i++) {
-        matrixPrint += itr->first;
-        matrixPrint += "\t";
-        for (int j = 0; j < realTypes.size(); j++) {
-            matrixPrint += to_string((int) (round(100 * ((mat[i][j]) / itr->second)))) + "%\t";
-        }
-        matrixPrint += "\n";
-        ++itr;
-    }
-    matrixPrint += ("k: " + to_string(this->classParams->getK()) +
-                    " metric: " + this->classParams->getMetric());
-    this->dio->write("$print$" + matrixPrint);
-    this->dio->read();
+//    std::fstream fin; // Opens the already-classified objects file.ד
+//    if (classified.empty()) {
+//        this->dio->write("Error!");
+//        return;
+//    }
+//    fin.open(this->classified, std::fstream::in);
+//    std::string line;
+//    while(getline(fin, line)) { // Loop over all the lines of the already-classified objects file.
+//        std::string type = ClassifierKnn::stringToClassifiable(&line).getType();
+//        auto it = find(types.begin(), types.end(), type); // Gets a vector of the types
+//        if (it == types.end()) {
+//            types.push_back(type);
+//        }
+//    }
+//
+//    fin.close(); // Closes the results file-stream.
 }
